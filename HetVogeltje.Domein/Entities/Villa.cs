f6c9a150b6ed7b7d1769db96ee2fb182460f19ec
Waivern <58275958+Waivern@users.noreply.github.com>
2025-08-11 00:19:32 +0200

    Nieuwe aanpassingen les 57 alweer

--- HetVogeltje.Domein/Entities/Villa.cs
+++ HetVogeltje.Domein/Entities/Villa.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace HetVogeltje.Domein.Entities
 {
@@ -10,10 +11,15 @@ namespace HetVogeltje.Domein.Entities
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public string?  Description { get; set; }
+        [Display(Name = "Omschrijving")]
+        public string? Description { get; set; }
+        [Display(Name = "Prijs per nacht")]
+        [Range(10,10000)]
         public double Price { get; set; }
         public int Sqft { get; set; }
+        [Range(1, 20)]
         public int Occupancy { get; set; }
+        [Display(Name = "Afbeelding link")]
         public string? ImagePath { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get;set; }
--- HetVogeltje.Web/Controllers/VillaController.cs
+++ HetVogeltje.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using HetVogeltje.Infrastructuur.Data;
+﻿using HetVogeltje.Domein.Entities;
+using HetVogeltje.Infrastructuur.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HetVogeltje.Web.Controllers
@@ -22,5 +23,64 @@ namespace HetVogeltje.Web.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+         if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("", "De naam en de omschrijving mogen niet hetzelfde zijn.");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View(obj);
+        }
+
+        //Edit
+        public IActionResult Update(int villaId)
+        {
+            Villa? villaFromDb = _context.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (villaFromDb == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        
+            return View(villaFromDb);
+
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("", "De naam en de omschrijving mogen niet hetzelfde zijn.");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View(obj);
+        }
+
+
+        //  Delete
+        public IActionResult Delete(int villaId)
+        {
+                       var villaFromDb = _context.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (villaFromDb == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+                _context.Villas.Remove(villaFromDb);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Villa");
+        }
+
     }
 }
