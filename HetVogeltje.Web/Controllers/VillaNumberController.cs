de3e4ceb8e7c88f0ef7b3329dcdb1d1287617c02
Waivern <Toneelvoormarcel@gmail.com>
2025-09-04 14:56:09 +0200

    aanpassingen

--- HetVogeltje.Web/Controllers/VillaNumberController.cs
+++ HetVogeltje.Web/Controllers/VillaNumberController.cs
@@ -40,20 +40,37 @@ namespace HetVogeltje.Web.Controllers
         public IActionResult Create(VillaNumberVM villaNumberVM)
         {
             //Controle of het huisnummer al bestaat
+            bool HuisnummerBestaat = _context.VillaNumbers.Any(u => u.Villa_Number == villaNumberVM.Huisnummer.Villa_Number);
 
-            if (_context.VillaNumbers.Any(u => u.Villa_Number == villaNumberVM.Huisnummer.Villa_Number))
-            {
-                TempData["Error"] = "Huisnummer: " + villaNumberVM.Huisnummer.Villa_Number + " bestaat al, aanmaken is niet gelukt.";
-                return RedirectToAction("Index");
-            }
-         //  ModelState.Remove("VillaNumber.Villa");// deze hoeft niet, omdat hij in de entity al niet wordt gecontroleerd.
-            if (ModelState.IsValid)
+
+            //Andere methode om te controleren of het huisnummer al bestaat
+            //if (_context.VillaNumbers.Any(u => u.Villa_Number == villaNumberVM.Huisnummer.Villa_Number))
+            //{
+            //    TempData["Error"] = "Huisnummer: " + villaNumberVM.Huisnummer.Villa_Number + " bestaat al, aanmaken is niet gelukt.";
+            //    return RedirectToAction("Index");
+            //}
+
+
+            //  ModelState.Remove("VillaNumber.Villa");// deze hoeft niet, omdat hij in de entity al niet wordt gecontroleerd.
+            if (ModelState.IsValid && !HuisnummerBestaat)
             {
                 _context.VillaNumbers.Add(villaNumberVM.Huisnummer);
                 _context.SaveChanges();
                 TempData["Success"] = "Het toevoegen van het huisnummer is gelukt.";
                 return RedirectToAction("Index");
             }
+
+            if(HuisnummerBestaat)
+            {
+                TempData["Error"] = "Huisnummer: " + villaNumberVM.Huisnummer.Villa_Number + " bestaat al, aanmaken is niet gelukt.";            
+            };
+            //Als het modelstate niet valid is, dan de lijst met villa's opnieuw ophalen
+            villaNumberVM.VillaList = _context.Villas.ToList().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+            //Vervolgens de view teruggeven met de ingevulde data van de viewmodel
             return View(villaNumberVM);
 
         }
@@ -61,16 +78,26 @@ namespace HetVogeltje.Web.Controllers
         
 
         //Edit
-        public IActionResult Update(int villaId)
+        public IActionResult Update(int villaNumberId)
         {
-            Villa? villaFromDb = _context.Villas.FirstOrDefault(u => u.Id == villaId);
-            if (villaFromDb == null)
+            VillaNumberVM villaNumberVM = new()
             {
-                TempData["Error"] = "Er is iets misgegaan bij het aanpassen van de villa.";
+                VillaList = _context.Villas.ToList().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                Huisnummer = _context.VillaNumbers.FirstOrDefault(u => u.Villa_Number == villaNumberId)
+            };
+                      
+
+
+            if (villaNumberId == null || villaNumberVM.Huisnummer == null)
+            {
+                TempData["Error"] = "Er is iets misgegaan bij het openen van de locatie.";
                 return RedirectToAction("Error", "Home");
             }
-        
-            return View(villaFromDb);
+            return View(villaNumberVM);
 
         }
 
