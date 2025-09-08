ce357074582cafb28179487a741fc82b0902edf9
Waivern <Toneelvoormarcel@gmail.com>
2025-09-08 15:55:17 +0200

    interface

---
+++ HetVogeltje.Application/Common/Interfaces/IVillaRepository.cs
@@ -0,0 +1,15 @@
+﻿using HetVogeltje.Domein.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HetVogeltje.Application.Common.Interfaces
+{
+    public interface IVillaRepository
+    {
+        IEnumerable<Villa> GetAll(Expression<Func<Villa,bool>>? filter = null,string? includeProperties = null);
+    }
+}
